5dae0fbc6c75f01a1838e904e1bd731032df861f
0xNaim <naimislam3545@gmail.com>
2022-08-19 22:49:30 +0600

    added middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,11 @@
-import { createStore } from "redux";
-import addProductReducer from "./addProduct/addProductReducer";
+import { applyMiddleware, createStore } from 'redux';
+import addProductReducer from './addProduct/addProductReducer';
 
-const store = createStore(addProductReducer);
+const validation = (store) => (next) => (action) => {
+  const itemsStore = store.getState();
+  
+};
+
+const store = createStore(addProductReducer, applyMiddleware(validation));
 
 export default store;
